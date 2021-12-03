f094ebb4c7762fd4728002ccd9db47d0d7e89fe1
SahilBhagchandani <sahilbhagchandani98@gmail.com>
2021-12-03 15:21:05 -0800

    final commit

--- react_202/src/webConfig.js
+++ react_202/src/webConfig.js
@@ -1,3 +1,3 @@
 // const backendServer = "http://localhost:3001";
-const backendServer = "http://3.19.66.129:3001";
+const backendServer = "http://18.117.217.121:3001";
 export default backendServer;
